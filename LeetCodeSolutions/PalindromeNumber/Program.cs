c9fcb4a7119ccc428d76a83d358b35581d10292f
Jeffrey Freet <jeffreyfreet@yahoo.com>
2022-05-17 15:28:52 -0400

    PalnindromeNumber
    Solution for Palindrome number

--- LeetCodeSolutions/PalindromeNumber/Program.cs
+++ LeetCodeSolutions/PalindromeNumber/Program.cs
@@ -6,7 +6,37 @@ namespace PalindromeNumber
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Solution(121);
+			Solution(-121);
+			Solution(10);
+		}
+
+		static void Solution(int x)
+		{
+			if (x < 0)
+			{
+				Console.WriteLine("False");
+			}
+
+			string number = x.ToString();
+			string newNumber = "";
+
+			for (int i = number.Length - 1; i >= 0; i--)
+			{
+				newNumber += number[i];
+			}
+
+
+			if (number.Equals(newNumber))
+			{
+				Console.WriteLine("True");
+			}
+			else
+			{
+				Console.WriteLine("False");
+			}
 		}
 	}
+
+
 }
